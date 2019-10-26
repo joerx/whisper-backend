7c5abe1fade6c749658086abb1bda7df93acfb08
Joerg Henning <1712460+joerx@users.noreply.github.com>
2019-10-26 17:18:32 +0800

    Use spring-data-rest

--- src/main/java/io/yodo/whisper/repository/ShoutJpaRepository.java
+++ src/main/java/io/yodo/whisper/repository/ShoutJpaRepository.java
@@ -2,6 +2,7 @@ package io.yodo.whisper.repository;
 
 import io.yodo.whisper.entity.Shout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface ShoutJpaRepository extends JpaRepository<Shout, Integer> {
 }

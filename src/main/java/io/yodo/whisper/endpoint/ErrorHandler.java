acbc4b7a833d87e9dafc3fa9d6e951ea3f6c1d33
Joerg Henning <1712460+joerx@users.noreply.github.com>
2019-11-11 09:48:29 +0800

    Update error handler

--- src/main/java/io/yodo/whisper/endpoint/ErrorHandler.java
+++ src/main/java/io/yodo/whisper/endpoint/ErrorHandler.java
@@ -3,6 +3,8 @@ package io.yodo.whisper.endpoint;
 import io.yodo.whisper.error.ErrorResponse;
 import io.yodo.whisper.error.InvalidRequestException;
 import io.yodo.whisper.error.NoSuchEntityException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -13,6 +15,8 @@ import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class ErrorHandler {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleError(ConstraintViolationException e) {
@@ -39,7 +43,13 @@ public class ErrorHandler {
         return createResponse(e, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
+    @ExceptionHandler
+    public  ResponseEntity<ErrorResponse> handleError(org.springframework.web.HttpMediaTypeNotSupportedException e) {
+        return createResponse(e, HttpStatus.NOT_ACCEPTABLE);
+    }
+
     private ResponseEntity<ErrorResponse> createResponse(Exception e, HttpStatus status) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<>(new ErrorResponse(e), status);
     }
 }
